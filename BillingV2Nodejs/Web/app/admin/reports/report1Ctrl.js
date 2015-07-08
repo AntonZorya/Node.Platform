f29cb225882420b08eee673d3cdab5b2abb762c9
nursultan156 <nursultan156@gmail.com>
2015-07-08 12:02:36 +0600

    asd

--- BillingV2Nodejs/Web/app/admin/reports/report1Ctrl.js
+++ BillingV2Nodejs/Web/app/admin/reports/report1Ctrl.js
@@ -42,6 +42,117 @@ function report1Ctrl(dataSvc, $scope, valSvc) {
 		}
 	}
 
+
+
+
+	var dashboardChartObptions = {
+		chart: {
+			renderTo: this,
+			backgroundColor: null,
+			borderWidth: 0,
+			type: 'area',
+			margin: [2, 0, 2, 0],
+			width: 1000,
+			height: 300,
+			style: {
+				overflow: 'visible'
+			},
+			skipClone: true
+		},
+		title: {
+			text: ''
+		},
+		credits: {
+			enabled: false
+		},
+		xAxis: {
+			labels: {
+				enabled: false
+			},
+			title: {
+				text: null
+			},
+			startOnTick: false,
+			endOnTick: false,
+			tickPositions: []
+		},
+		yAxis: {
+			endOnTick: false,
+			startOnTick: false,
+			labels: {
+				enabled: false
+			},
+			title: {
+				text: null
+			},
+			tickPositions: [0]
+		},
+		legend: {
+			enabled: false
+		},
+		tooltip: {
+			backgroundColor: null,
+			borderWidth: 0,
+			shadow: false,
+			useHTML: true,
+			hideDelay: 0,
+			shared: true,
+			padding: 0,
+			positioner: function (w, h, point) {
+				return {x: point.plotX - w / 2, y: point.plotY - h};
+			}
+		},
+		plotOptions: {
+			series: {
+				animation: false,
+				lineWidth: 1,
+				shadow: false,
+				states: {
+					hover: {
+						lineWidth: 1
+					}
+				},
+				marker: {
+					radius: 1,
+					states: {
+						hover: {
+							radius: 2
+						}
+					}
+				},
+				fillOpacity: 0.25
+			},
+			column: {
+				negativeColor: '#910000',
+				borderColor: 'silver'
+			}
+		}
+
+
+	};
+
+
+	$scope.karimovChartConfig = {
+		options: dashboardChartObptions,
+		series: [{
+			data: [4, 5, 0, 12, 18],
+			tooltip: {
+
+				headerFormat: "",
+				pointFormat: 'Даяров Алмабек :{point.y}<br>'
+			}
+		},
+
+			{
+			data: [9, 0, 4, 5, 12],
+			tooltip: {
+
+				headerFormat: "",
+				pointFormat: 'Бекбалаев Максат :{point.y}<br>'
+			}}
+		]
+	}
+
 	
 
 }
