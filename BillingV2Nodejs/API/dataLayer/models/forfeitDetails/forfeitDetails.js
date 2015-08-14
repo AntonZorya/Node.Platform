209451c99f9274693302e84c58c509ec376610fe
Dmitriy Klimovich <klimovichdr@gmail.com>
2015-08-14 20:24:39 +0600

    forfeit.js

---
+++ BillingV2Nodejs/API/dataLayer/models/forfeitDetails/forfeitDetails.js
@@ -0,0 +1,17 @@
+var ModelBase = require('../base/modelBase');
+var mongoose = require('mongoose');
+var Schema = mongoose.Schema;
+var _ = require('underscore');
+//Штрафы
+exports.definition = _.extend({
+
+    balanceId: {type: Schema.Types.ObjectId, ref: 'Balance', required: true},
+    comment: {type: String},
+    filesIds: {type: Array}
+
+    /*clientJurId: {type: Schema.Types.ObjectId, ref: 'ClientJur'},
+     clientFizId: {type: Schema.Types.ObjectId, ref: 'ClientFiz'},
+     sum: {type: Number},
+     date: {type: Date},*/
+
+}, ModelBase);
