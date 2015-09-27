125694fe1b2ac8196b1ed2c5e14eca633abd5269
Ivan <SetInNirvana@gmail.com>
2015-09-28 00:24:54 +0600

    clientFiz done

--- BillingV2Nodejs/API/dataLayer/models/client/clientFiz.js
+++ BillingV2Nodejs/API/dataLayer/models/client/clientFiz.js
@@ -15,15 +15,15 @@ exports.definition = _.extend({
 	bin: {type: String, required: "#bin required"},
 	rnn: {type: String},
 
-	addressId: {type: Schema.Types.ObjectId, ref: 'Address', required: true},
+	addressId: {type: Schema.Types.ObjectId, required: true},
 	address: {type: String, required: "#address required"},
 	phone: {type: String},
 	email: {type: String},
 	period: {type: Number},
 	pipelines: clientPipelines,
 
-	controllerId: {type: Schema.Types.ObjectId, ref: 'Controller', required: true},
-	clientType: clientType,
+	controllerId: {type: Schema.Types.ObjectId, required: true},
+	//clientType: clientType,
 	kskId: {type: Schema.Types.ObjectId, ref: 'ksk'},
 	abonentEntryDate: {type: Date},
 
