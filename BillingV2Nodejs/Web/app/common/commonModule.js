45004596425f187efa8079e2bad15321c84c02b4
Иван <setinnirvana@gmail.com>
2015-10-05 16:39:30 +0600

    settings

--- BillingV2Nodejs/Web/app/common/commonModule.js
+++ BillingV2Nodejs/Web/app/common/commonModule.js
@@ -1,3 +1,3 @@
 var configModule = angular.module('commonModule',[])
-    .constant('WEB_HOST', 'http://localhost:9999')
-    .constant('API_HOST', 'http://localhost:8080/API');
+    .constant('WEB_HOST', 'http://89.218.66.54:9080')
+    .constant('API_HOST', 'http://89.218.66.54:8080/API');
